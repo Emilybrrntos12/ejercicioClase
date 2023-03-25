0af1868170b0559948a3074d2b73f9b60abf1012
Emilybrrntos12 <100007089+Emilybrrntos12@users.noreply.github.com>
2023-03-25 09:03:57 -0600

    Add files via upload

---
+++ clase2503/src/main/java/listas/main.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package listas;
+
+/**
+ *
+ * @author Emily Barrientos
+ */
+public class main {
+    
+    public static void main(String[] args) {
+        Lista l = new Lista();
+        var respuesta = l.crearLista();
+        //System.out.println(respuesta);
+        respuesta.visualizar();
+    }
+
+}
